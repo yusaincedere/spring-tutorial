0c6b1541d210955a6751154d94deee0b54719c99
yusaincedere <yusaincedere@gmail.com>
2022-07-24 13:08:48 +0300

    v1

---
+++ src/main/java/com/example/demo/models/Student.java
@@ -0,0 +1,81 @@
+package com.example.demo.models;
+
+import java.time.LocalDate;
+
+public class Student {
+    private long id;
+    private String name;
+    private String email;
+    private int age;
+    private LocalDate dob;
+
+    public Student() {
+
+    }
+
+    public Student(long id, String name, String email, int age,LocalDate dob) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dob = dob;
+    }
+
+    public Student(String name, String email, int age,LocalDate dob) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dob = dob;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", dob=" + dob +
+                '}';
+    }
+}
